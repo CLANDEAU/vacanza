6fa3b2d890a43c3aec5abd70b0fa2e847be243a3
CLANDEAU <92865093+CLANDEAU@users.noreply.github.com>
2026-03-30 19:13:54 +0200

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,1 +1,4 @@
+const supabaseUrl = "https://vvyujtzftcdbfkzvusbh.supabase.co"
+const supabaseKey = "sb_publishable_nHB_OJQkdUlDht5716YXxA_mMcfFOLL"
 
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
